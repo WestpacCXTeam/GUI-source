358cc9991f5075f8f7d0ba79a2d1596957788edb
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2015-05-27 17:25:31 +1000

    fixed basefont

--- accessibility-helper/Gruntfile.js
+++ accessibility-helper/Gruntfile.js
@@ -173,17 +173,17 @@ module.exports = function(grunt) {
 
 			//copy font assets
 			brands.forEach(function(brand) {
-				copy[ version + 'Font' + brand ] = {
+				copy[ version + 'BaseFont' + brand ] = {
 					expand: true,
-					flatten: true,
-					src: '../base/' + baseVersion + '/_assets/' + brand + '/font/*',
-					dest: './' + version + '/tests/' + brand + '/assets/font/',
+					cwd: '../base/' + baseVersion + '/_assets/' + brand + '/font',
+					src: '*',
+					dest: './' + version + '/tests/' + brand + '/assets/font',
 				};
 
 				copy[ version + 'Font' + brand ] = {
 					expand: true,
-					flatten: true,
-					src: './' + version + '/_assets/' + brand + '/font/',
+					cwd: './' + version + '/_assets/' + brand + '/font',
+					src: '*',
 					dest: './' + version + '/tests/' + brand + '/assets/font',
 				};
 			});
@@ -206,6 +206,8 @@ module.exports = function(grunt) {
 
 
 			//handle svgs
+			var svgselectors = grunt.file.readJSON('./' + version + '/_assets/grunticon.json');
+
 			brands.forEach(function(brand) {
 				grunticon[ version + 'SVG' + brand ] = {
 					files: [{
@@ -222,12 +224,7 @@ module.exports = function(grunt) {
 						cssprefix: '.symbol-',
 						pngpath: '../img',
 						enhanceSVG: true,
-						customselectors: {
-							// 'radio-on': ['input[type="radio"]:checked + label'],
-							// 'radio-off': ['.radio label', '.radio-inline label'],
-							// 'checkbox-on': ['input[type="checkbox"]:checked + label'],
-							// 'checkbox-off': ['.checkbox label', '.checkbox-inline label'],
-						},
+						customselectors: svgselectors,
 					},
 				};
 
