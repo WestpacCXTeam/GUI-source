affa2c48bda968b55829dfaa4726ab897ba20027
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2015-06-04 15:11:20 +1000

    added styles

--- modals/1.0.0/tests/BOM/assets/js/gui.js
+++ modals/1.0.0/tests/BOM/assets/js/gui.js
@@ -83,25 +83,91 @@ App.init();
  **************************************************************************************************************************************************************/
 
 
-(function(App) {
+(function(GUI) {
 
 	var module = {};
 
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// public API function: open / close a modal
+	//
+	// _isOpen  [boolean]        Whether to open or close the modal
+	// $modal   [jquery object]  The modal element
+	// target   [string]         Selector string to id the modal for opening only
+	//------------------------------------------------------------------------------------------------------------------------------------------------------------
+	module.toggelModal = function toggelModal(_isOpen, $modal, target) {
+		GUI.debugging( 'modals: ' + ( _isOpen ? 'Closing' : 'Opening' ) + ' modal', 'report' );
+
+		if( _isOpen ) {
+			$modal
+				.removeClass('is-open')
+				.attr('aria-hidden', 'true');
+
+			$('.modal-backdrop').remove();
+		}
+		else {
+			$modal
+				.addClass('is-open')
+				.attr('aria-hidden', 'false')
+				.focus();
+
+			$modal.after('<div class="modal-backdrop js-modalclose" data-modal="' + target + '"></div>')
+		}
+
+	}
+
+	//------------------------------------------------------------------------------------------------------------------------------------------------------------
 	// module init method
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
-	module.init = function() {
-		App.debugging( 'modals: Initiating', 'report' );
+	module.init = function modalInit() {
+		GUI.debugging( 'modals: Initiating', 'report' );
 
 
-		// some js stuff
+		if( $('.js-modal').length ) {
+			GUI.debugging( 'modals: Found instances', 'report' );
+
+			//open button click
+			$('.js-modal').on('click', function openModal() {
+				GUI.debugging( 'modals: open button clicked', 'interaction' );
+
+				var $this = $(this);
+				var target = $this.attr('data-modal');
+				var $modal = $( target );
+				var _isOpen = $modal.hasClass('is-open');
+
+				GUI.modals.toggelModal(_isOpen, $modal, target);
+			});
+
+
+			//close button click
+			$(document).on('click', '.js-modalclose', function openModal() {
+				GUI.debugging( 'modals: close button / backdrop clicked', 'interaction' );
+
+				var $this = $(this);
+				var $modal = $('.modal');
+
+				GUI.modals.toggelModal(true, $modal, '');
+			});
+
+
+			//ESC button listener
+			$(document).keyup(function escapeKey(e) {
+				if(e.keyCode == 27) {
+					GUI.debugging( 'modals: Esc button clicked', 'interaction' );
+
+					var $modal = $('.modal');
+
+					GUI.modals.toggelModal(true, $modal, '');
+				}
+			});
+
+		}
 	};
 
 
-	App.modals = module;
+	GUI.modals = module;
 
 
 	// run module
-	App.modals.init();
+	GUI.modals.init();
 
-}(App));
+}(GUI));
