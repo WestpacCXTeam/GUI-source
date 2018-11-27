88f14ad169f1af2d8af41d739a093e4aae5438e4
Jonny Stening <info@jonathanstening.com>
2018-11-27 11:55:06 +1100

    updating module _template files

--- ._template/module/js/module.js
+++ ._template/module/js/module.js
@@ -14,12 +14,30 @@
 
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
 	// module init method
+	//
+	// Run this method once after your DOM was loaded
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
-	module.init = function() {
+	module.init = function [-Module-]Init() {
 		GUI.debugging( '[-Module-]: Initiating', 'report' );
 
+		// Check the module exists on the page
+		if( $('.js-[-Module-]').length ) {
+			GUI.debugging( '[-Module-]: Found instance', 'report' );
 
-		// some js stuff
+			GUI.[-Module-].render();
+		}
+	};
+
+
+	//------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// module render method
+	//
+	// You can run the render method if you want to bypass the length check or render elements added dynamically to the DOM after loading
+	//------------------------------------------------------------------------------------------------------------------------------------------------------------
+	module.render = function [-Module-]Render() {
+		GUI.debugging( '[-Module-]: Rendering', 'report' );
+
+		// Some js stuff...
 	};
 
 
